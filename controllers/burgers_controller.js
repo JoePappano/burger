f3c0b4ff343e047b0a3ffe0a9e1d0d5b2d71c374
Joseph Pappano <jepappano4@Josephs-MacBook-Pro.local>
2019-01-28 21:38:26 -0600

    updating files

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -4,4 +4,50 @@ var burger = require("../models/burger.js");
 
 var router = express.Router();
 
+router.get("/", function(req, res) {
+    burger.selectAll(function(data) {
+      var hbsObject = {
+        cats: data
+      };
+      console.log(hbsObject);
+      res.render("index", hbsObject);
+    });
+  });
+  
+  router.post("/create", function(req, res) {
+    burger.create(["burger_name"], [req.body.burger], function(result) {
+        res.redirect("/");
+    });
+  });
+  
+  router.post("/update", function(req, res) {
+    var condition = "id = " + req.params.id;
+  
+    console.log("condition", condition);
+  
+//     burger.updateOne({
+//       sleepy: req.body.sleepy
+//     }, condition, function(result) {
+//       if (result.changedRows == 0) {
+//         // If no rows were changed, then the ID must not exist, so 404
+//         return res.status(404).end();
+//       } else {
+//         res.status(200).end();
+//       }
+//     });
+//   });
+  
+//   router.delete("/api/cats/:id", function(req, res) {
+//     var condition = "id = " + req.params.id;
+  
+//     cat.delete(condition, function(result) {
+//       if (result.affectedRows == 0) {
+//         // If no rows were changed, then the ID must not exist, so 404
+//         return res.status(404).end();
+//       } else {
+//         res.status(200).end();
+//       }
+//     });
+//   });
+
 module.exports = controller;
--- models/burger.js
+++ models/burger.js
@@ -1,9 +1,31 @@
 var orm = require("../config/orm.js");
 
-//orm.selectAll() { }
-
-//orm.insertOne() { }
-
-//orm.updateOne() { }
+var burger = {
+    selectAll: function(cb) {
+        orm.selectAll("burgers", function(res) {{
+            cb(res);
+        }})
+      },
+      insertOne: function(table, cols, vals, cb) {
+        orm.insertOne("burgers", cols, vals, function(res) {
+            cb(res);
+        })
+      },
+      updateOne: function(table, objColVals, condition, cb) {
+        orm.updateOne("burgers", objColVals, condition, function(res) {
+            cb(res);
+        });
+      },
+      updateTwo: function(table, objColVals, condition, cb) {
+        orm.updateTwo("burgers", objColVals, condition, function(res) {
+            cb(res);
+        });
+      },
+      delete: function(table, condition, cb) {
+          orm.delete("burgers", condition, function(res) {
+            cb(res);
+          });
+      }
+}
 
 module.exports = burger;
