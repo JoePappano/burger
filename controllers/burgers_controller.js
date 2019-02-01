5c1f04652e97b7cb47f8b40a00d6977944689cf9
Joseph Pappano <jepappano4@Josephs-MacBook-Pro.local>
2019-01-31 18:32:29 -0600

    updating files

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -35,17 +35,18 @@ router.get("/", function(req, res) {
     });
   });
   
-//   router.delete("/api/cats/:id", function(req, res) {
-//     var condition = "id = " + req.params.id;
-  
-//     cat.delete(condition, function(result) {
-//       if (result.affectedRows == 0) {
-//         // If no rows were changed, then the ID must not exist, so 404
-//         return res.status(404).end();
-//       } else {
-//         res.status(200).end();
-//       }
-//     });
-//   });
+  router.post("/delete", function(req, res) {
+    var condition = "id = " + req.body.id;
+    console.log("condition: " + condition)
+    burger.delete(condition, function(result) {
+      if (result.affectedRows == 0) {
+        // If no rows were changed, then the ID must not exist, so 404
+        return res.status(404).end();
+      } else {
+        res.status(200).end();
+      }
+    });
+    res.redirect("/");
+  });
 
 module.exports = router;
