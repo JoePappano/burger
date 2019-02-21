b3fd3d60fab525a5f72622dde10a2ff41be12136
Joseph Pappano <jepappano4@Josephs-MacBook-Pro.local>
2019-02-20 21:03:16 -0600

    make it better

--- config/connection.js
+++ config/connection.js
@@ -1,21 +1,27 @@
 var mysql = require("mysql");
-if (process.env.PORT) {
-    var connection = mysql.createConnection({
-        host: "d13xat1hwxt21t45.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-        port: 3306,
-        user: "dqt06yo9af7t9r4h",
-        password: "vjux9fihsl2idf1h",
-        database: "tupvkrrvao1v8hg8"
-    });
-} else {
+// if (process.env.PORT) {
+//     var connection = mysql.createConnection({
+//         host: "d13xat1hwxt21t45.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+//         port: 3306,
+//         user: "dqt06yo9af7t9r4h",
+//         password: "vjux9fihsl2idf1h",
+//         database: "tupvkrrvao1v8hg8",
+//         production: {
+//             "use_env_variable": "JAWSDB_URL",
+//             "dialect": "mysql"
+//         }
+//     });
+// } else {
     var connection = mysql.createConnection({
         host: "localhost",
         port: 8889,
         user: "root",
         password: "",
-        database: "burgers_db"
+        database: "burgers_db",
+        use_env_variable: "JAWSDB_URL",
+        dialect: "mysql"
     });
-}
+// }
 
 connection.connect(function(err) {
     if (err){
