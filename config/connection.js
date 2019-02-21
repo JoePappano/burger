ff82a6098f9d04e5df6e17b6c8358aa0e50a4c7f
Joseph Pappano <jepappano4@Josephs-MacBook-Pro.local>
2019-02-20 21:26:02 -0600

    updating files

--- config/connection.js
+++ config/connection.js
@@ -1,17 +1,15 @@
 var mysql = require("mysql");
-// if (process.env.PORT) {
-//     var connection = mysql.createConnection({
-//         host: "d13xat1hwxt21t45.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-//         port: 3306,
-//         user: "dqt06yo9af7t9r4h",
-//         password: "vjux9fihsl2idf1h",
-//         database: "tupvkrrvao1v8hg8",
-//         production: {
-//             "use_env_variable": "JAWSDB_URL",
-//             "dialect": "mysql"
-//         }
-//     });
-// } else {
+if (process.env.PORT) {
+    var connection = mysql.createConnection({
+        host: "d13xat1hwxt21t45.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+        port: 3306,
+        user: "dqt06yo9af7t9r4h",
+        password: "vjux9fihsl2idf1h",
+        database: "tupvkrrvao1v8hg8",
+        use_env_variable: "JAWSDB_URL",
+        dialect: "mysql"
+    });
+} else {
     var connection = mysql.createConnection({
         host: "localhost",
         port: 8889,
@@ -21,7 +19,7 @@ var mysql = require("mysql");
         use_env_variable: "JAWSDB_URL",
         dialect: "mysql"
     });
-// }
+}
 
 connection.connect(function(err) {
     if (err){
