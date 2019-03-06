6d64d55b1ed588317a3a2c2e1c54ca51a1ff5b16
Joseph Pappano <jepappano4@Josephs-MacBook-Pro.local>
2019-03-05 18:03:47 -0600

    fixing database

--- config/connection.js
+++ config/connection.js
@@ -3,11 +3,11 @@ if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
     var connection = mysql.createConnection({
-        host: "localhost",
-        port: 8889,
-        user: "root",
-        password: "",
-        database: "burgers_db",
+        host: "d13xat1hwxt21t45.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+        port: 3306,
+        user: "dqt06yo9af7t9r4h",
+        password: "vjux9fihsl2idf1h",
+        database: "	tupvkrrvao1v8hg8",
         use_env_variable: "JAWSDB_URL",
         dialect: "mysql"
     });
